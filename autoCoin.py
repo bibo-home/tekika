fc5afbdd10edd5e50f9a32e951940e1d21c9cdd4
Trung Nguyen <nguyencanhtrung@me.com>
2024-09-06 21:15:28 -0400

    Add import os
    Signed-off-by: Trung Nguyen <nguyencanhtrung@me.com>

--- autoCoin.py
+++ autoCoin.py
@@ -1,6 +1,8 @@
 
 import platform
 import time
+import os
+import json
 from webDriverLib import WebDriverLibrary, ConfigReader
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
